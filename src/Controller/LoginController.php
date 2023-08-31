c40d212462ef6d53d7fb680747d9e4297339f8f8
Mina ZEKRI <mina.zekri2022@campus-eni.fr>
2023-08-31 20:28:48 +0200

    modification tout code

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -27,7 +27,6 @@ class LoginController extends AbstractController
     #[Route('/logout', name: 'app_logout')]
     public function logout(): never
     {
-        // controller can be blank: it will never be called!
         throw new \Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
